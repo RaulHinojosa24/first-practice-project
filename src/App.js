1c81645cf5f8c29665d346dab86fb63a55025ba5
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-08-26 11:55:25 +0200

    card component and basic styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Card from "./UI/Card";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Card>
+        <h1>Text</h1>
+      </Card>
+    </div>
+  );
 }
 
 export default App;
